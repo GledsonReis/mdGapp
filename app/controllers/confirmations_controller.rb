6edb5e4d16b5873cd22b69e44633c7072b204b16
Gledson Reis <gledsonthegreeder@gmail.com>
2020-01-16 16:51:14 -0300

    adding new fields to user

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    sing_in(resource)
-    dashboard_path
+    sign_in(resource)
+    dashboard_index_path
   end
 end
