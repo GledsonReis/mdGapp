8ba37e50625e56d65ed681b29dc682fe018037e7
Gledson Reis <gledsonthegreeder@gmail.com>
2020-02-17 17:45:24 -0300

    devise css layout

--- app/javascript/packs/auth.js
+++ app/javascript/packs/auth.js
@@ -17,7 +17,6 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "bootstrap";
-import "../stylesheets/application";
 import "../stylesheets/auth";
 import "./custom";
 import "@fortawesome/fontawesome-free/js/all";
