3ba186c2e4afa473267f65c0ea8f9fb3692e889e
Gledson Reis <gledsonthegreeder@gmail.com>
2020-02-27 18:02:21 -0300

    css corretions

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -5,3 +5,16 @@
 // document.addEventListener("turbolinks:load", () => {
 //   $("[data-toggle='popover']").tooltip()
 // });
+
+$(document).ready(function(){
+  $(".opensidebar").on("click", function() {
+    console.log("gg");
+    $("#mySidenav").css("width", "100%");
+  });
+  
+  /* Close/hide the sidenav */
+  $(".closebtn").on("click", function() {
+    $("#mySidenav").css("width", "0");
+  });
+
+});
