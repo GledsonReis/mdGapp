a000da9b31864a90628b2021b4737b2262f66b55
Gledson Reis <gledsonthegreeder@gmail.com>
2020-02-28 11:23:14 -0300

    corrigindo viewport

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -6,15 +6,15 @@
 //   $("[data-toggle='popover']").tooltip()
 // });
 
-$(document).ready(function(){
+var ready = function() {
   $(".opensidebar").on("click", function() {
-    console.log("gg");
     $("#mySidenav").css("width", "100%");
   });
   
   /* Close/hide the sidenav */
   $(".closebtn").on("click", function() {
     $("#mySidenav").css("width", "0");
-  });
+  })
+};
 
-});
+$(document).on('turbolinks:load', ready); 
