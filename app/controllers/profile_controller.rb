48e3836b54e6953ef8843765769c04abb78580a7
Gledson Reis <gledsonthegreeder@gmail.com>
2020-03-13 11:35:20 -0300

    profile view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,6 @@ class ProfileController < AuthController
   def index
     @user = User.find(params[:id])
     @can_follow = already_follow(@user)
+    @posts = Post.where(user_id: @user.id)
   end
 end
